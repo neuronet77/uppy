84342f85153179842001ef78c0efc3337a15f31c
neuronet77 <a.hinderberger@yahoo.ca>
2023-04-30 13:16:21 -0500

    Update server.php
    Added some sanitizing and basic validation to the server script

--- examples/php-xhr/server.php
+++ examples/php-xhr/server.php
@@ -3,9 +3,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Define allowed file types and maximum size
+$allowed_types = array('image/jpeg', 'image/png', 'application/pdf');
+$max_size = 10 * 1024 * 1024; // 10 MB
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH');
     }
     //Make sure you remove those you do not want to support
     header('Access-Control-Allow-Origin: *');
@@ -18,14 +22,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
 
-if ($_POST && !empty($_FILES["file"])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_FILES["file"])) {
     $target_dir = __DIR__ . DIRECTORY_SEPARATOR . 'uploads';
-    $target_file = $target_dir . DIRECTORY_SEPARATOR . basename($_FILES['file']['name']);
+    $file_name = basename($_FILES['file']['name']);
+    $file_type = $_FILES['file']['type'];
+    $file_size = $_FILES['file']['size'];
+    $target_file = $target_dir . DIRECTORY_SEPARATOR . $file_name;
+
+    // Validate file type and size
+    if (!in_array($file_type, $allowed_types)) {
+        header('Access-Control-Allow-Origin: *');
+        header('Content-type: application/json');
+        $data = ['message' => 'Invalid file type. Only JPEG, PNG, and PDF files are allowed.'];
+        http_response_code(400);
+        echo json_encode($data);
+        exit;
+    }
+
+    if ($file_size > $max_size) {
+        header('Access-Control-Allow-Origin: *');
+        header('Content-type: application/json');
+        $data = ['message' => 'File size exceeds the maximum allowed size of 10 MB.'];
+        http_response_code(400);
+        echo json_encode($data);
+        exit;
+    }
+
+    // Sanitize file name to prevent directory traversal attacks
+    $file_name = preg_replace('/[^a-zA-Z0-9._-]/', '', $file_name);
+    $target_file = $target_dir . DIRECTORY_SEPARATOR . $file_name;
+
     try {
         if (move_uploaded_file($_FILES['file']['tmp_name'], $target_file)) {
             header('Access-Control-Allow-Origin: *');
             header('Content-type: application/json');
-            $data = ['url' => $target_file, 'message' => 'The file ' . basename($_FILES['file']['name']) . ' has been uploaded.'];
+            $data = ['url' => $target_file, 'message' => 'The file ' . $file_name . ' has been uploaded.'];
             http_response_code(201);
             echo json_encode($data);
         } else {
@@ -39,4 +70,10 @@ if ($_POST && !empty($_FILES["file"])) {
         http_response_code(400);
         echo json_encode($data);
     }
+} else {
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json');
+    $data = ['message' => 'Please upload a file.'];
+    http_response_code(400);
+    echo json_encode($data);
 }
